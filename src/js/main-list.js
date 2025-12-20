43ea43a6b1bb769c26fda8acf7a2fb1418b30f44
allivan2020 <allivan2020@gmail.com>
2025-12-20 17:03:36 +0200

    make khanges

--- src/js/main-list.js
+++ src/js/main-list.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import simpleParallax from 'simple-parallax-js/vanilla';
+
+const images = document.querySelectorAll('.parallax-img');
+
+new simpleParallax(images, {
+  scale: 1.2,
+  delay: 0.6,
+  overflow: true,
+});

7b98b5c0ca7f65023b968cf106782c1ee9a55b8d
allivan2020 <allivan2020@gmail.com>
2026-01-28 20:22:58 +0200

    make chsnge

--- src/js/main-list.js
+++ src/js/main-list.js
@@ -1,12 +1,10 @@
-import '../css/header.css';
-
 const menuBtn = document.querySelector('.menu-btn');
 const hiddenMenu = document.querySelector('.hiden-menu');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
 menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active'); // кнопка превращается в крестик
-  hiddenMenu.classList.toggle('is-open'); // меню открывается
+  menuBtn.classList.toggle('active'); // превращаем кнопку в крестик
+  hiddenMenu.classList.toggle('is-open'); // открываем/закрываем overlay
 });
 
 menuBtnClose.addEventListener('click', () => {
